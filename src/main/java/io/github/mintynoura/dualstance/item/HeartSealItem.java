79f2e8fd1f1caf8236a8dd13e6a2f000b2965f0f
Tau's PC <yarinhalali2015@gmail.com>
2026-04-13 21:30:05 +0300

    Start adding linking

--- src/main/java/io/github/mintynoura/dualstance/item/HeartSealItem.java
+++ src/main/java/io/github/mintynoura/dualstance/item/HeartSealItem.java
@@ -1,9 +1,33 @@
 package io.github.mintynoura.dualstance.item;
 
-import net.minecraft.world.item.BundleItem;
+import io.github.mintynoura.dualstance.registries.DualStanceComponents;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
-public class HeartSealItem extends BundleItem {
+public class HeartSealItem extends Item {
 	public HeartSealItem(Properties properties) {
 		super(properties);
+	}
+	//TODO: Implement adding crest
+	@Override
+	public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity target, InteractionHand type) {
+		if(player.level().isClientSide() || !(target instanceof Player otherPlayer))
+			return InteractionResult.FAIL;
+		ItemStack otherItemStack = otherPlayer.getItemInHand(InteractionHand.MAIN_HAND);
+		if(!(otherItemStack.getItem() instanceof HeartSealItem))
+			return InteractionResult.FAIL;
+
+		otherItemStack.set(DualStanceComponents.LINKED_PLAYER, player.getUUID());
+		itemStack.set(DualStanceComponents.LINKED_PLAYER, otherPlayer.getUUID());
+		player.makeSound(SoundEvents.ENDER_DRAGON_DEATH);
+		// TODO: Add crest effects to each other's
+		//otherItemStack.set(DualStanceComponents.LINKED_CREST, )
+
+		return InteractionResult.SUCCESS;
 	}
 }
--- src/main/java/io/github/mintynoura/dualstance/registries/DualStanceComponents.java
+++ src/main/java/io/github/mintynoura/dualstance/registries/DualStanceComponents.java
@@ -4,12 +4,31 @@ import io.github.mintynoura.dualstance.DualStance;
 import io.github.mintynoura.dualstance.component.CrestComponent;
 import net.fabricmc.fabric.api.item.v1.ItemComponentTooltipProviderRegistry;
 import net.minecraft.core.Registry;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.UUID;
 
 public class DualStanceComponents {
-	public static final DataComponentType<CrestComponent> CREST_COMPONENT = Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, Identifier.fromNamespaceAndPath(DualStance.ID, "crest"), DataComponentType.<CrestComponent>builder().persistent(CrestComponent.CODEC).build());
+	// This data component holds the crest's effect on the crest item
+	public static final DataComponentType<CrestComponent> CREST_COMPONENT =
+		Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, Identifier.fromNamespaceAndPath(DualStance.ID, "crest"),
+			DataComponentType.<CrestComponent>builder().persistent(CrestComponent.CODEC).build());
+
+	// This data component holds the linked player on the other player's heart seal
+	public static final DataComponentType<UUID> LINKED_PLAYER = Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE,
+		Identifier.fromNamespaceAndPath(DualStance.ID,"linked_player"),
+		DataComponentType.<UUID>builder().persistent(UUIDUtil.CODEC).build());
+
+	// This data component holds the linked crest effect on the other player's heart seal
+	public static final DataComponentType<CrestComponent> LINKED_CREST =
+		Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, Identifier.fromNamespaceAndPath(DualStance.ID, "linked_crest"),
+			DataComponentType.<CrestComponent>builder().persistent(CrestComponent.CODEC).build());
+
 	public static void initialize() {
 		ItemComponentTooltipProviderRegistry.addFirst(CREST_COMPONENT);
 	}
