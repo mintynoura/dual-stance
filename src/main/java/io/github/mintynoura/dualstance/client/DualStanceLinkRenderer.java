6cf23f7755b02856befc1d834437af3fa2a02b79
mintynoura <mintynoura@gmail.com>
2026-04-14 21:12:10 +0300

    moved renderer to client directory

---
+++ src/main/java/io/github/mintynoura/dualstance/client/DualStanceLinkRenderer.java
@@ -0,0 +1,10 @@
+package io.github.mintynoura.dualstance.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.feature.LeashFeatureRenderer;
+
+@Environment(EnvType.CLIENT)
+public class DualStanceLinkRenderer extends LeashFeatureRenderer {
+
+}
