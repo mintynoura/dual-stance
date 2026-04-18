0a0e7348ac8e27838109a2d2bfe1589cc426c0bf
Tau's PC <yarinhalali2015@gmail.com>
2026-04-18 13:33:29 +0300

    Adding to Creative Menu

--- src/main/java/io/github/mintynoura/dualstance/registries/DualStanceItems.java
+++ src/main/java/io/github/mintynoura/dualstance/registries/DualStanceItems.java
@@ -6,6 +6,7 @@ import io.github.mintynoura.dualstance.item.component.CrestComponent;
 import io.github.mintynoura.dualstance.item.CrestItem;
 import io.github.mintynoura.dualstance.item.HeartSealItem;
 import io.github.mintynoura.dualstance.item.component.HeartSealedCrest;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -13,6 +14,8 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
 import java.util.List;
@@ -21,7 +24,7 @@ import java.util.function.Function;
 public class DualStanceItems {
 	public static final Item HEART_SEAL = registerItem("heart_seal", HeartSealItem::new, new Item.Properties()
 		.component(DualStanceComponents.HEART_SEALED_CREST, HeartSealedCrest.EMPTY)
-		.stacksTo(1));
+		.stacksTo(1), CreativeModeTabs.COMBAT);
 
 	public static final Item PEACE_CREST = registerItem("peace_crest", CrestItem::new, new Item.Properties()
 		// we should probably make a helper method to make the crest components easier to read/write
@@ -31,12 +34,15 @@ public class DualStanceItems {
 				new AttributeCrestEffect.Entry(Attributes.ARMOR, new AttributeModifier(Identifier.fromNamespaceAndPath(DualStance.ID, "peace_crest"), 1.0f, AttributeModifier.Operation.ADD_VALUE)),
 				new AttributeCrestEffect.Entry(Attributes.ARMOR_TOUGHNESS, new AttributeModifier(Identifier.fromNamespaceAndPath(DualStance.ID, "peace_crest"), 1.0f, AttributeModifier.Operation.ADD_VALUE)
 				))
-			)))));
+			)))), CreativeModeTabs.COMBAT);
 
-	public static Item registerItem(String name, Function<Item.Properties, Item> factory, Item.Properties settings) {
+	public static Item registerItem(String name, Function<Item.Properties, Item> factory, Item.Properties settings, ResourceKey<CreativeModeTab> tab) {
 		ResourceKey<Item> itemRegistryKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(DualStance.ID, name));
 		Item item = factory.apply(settings.setId(itemRegistryKey));
 		Registry.register(BuiltInRegistries.ITEM, itemRegistryKey, item);
+		CreativeModeTabEvents.modifyOutputEvent(tab).register(creativeTab -> {
+			creativeTab.accept(item);
+		});
 		return item;
 	}
 	public static void initialize() {}
