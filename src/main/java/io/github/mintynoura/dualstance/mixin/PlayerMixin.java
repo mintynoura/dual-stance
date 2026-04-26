2a041bc9daf387e353bd98258485e766e95855b6
mintynoura <mintynoura@gmail.com>
2026-04-26 21:10:32 +0300

    pacifist damage mixins

--- src/main/java/io/github/mintynoura/dualstance/mixin/PlayerMixin.java
+++ src/main/java/io/github/mintynoura/dualstance/mixin/PlayerMixin.java
@@ -1,10 +1,11 @@
 package io.github.mintynoura.dualstance.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import io.github.mintynoura.dualstance.registries.DualStanceComponents;
-import net.minecraft.world.entity.Avatar;
-import net.minecraft.world.entity.ContainerUser;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
+import io.github.mintynoura.dualstance.registries.DualStanceItems;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -12,8 +13,7 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends Avatar implements ContainerUser {
@@ -24,16 +24,31 @@ public abstract class PlayerMixin extends Avatar implements ContainerUser {
 		super(type, level);
 	}
 
-	@Inject(method = "canHarmPlayer", at = @At("HEAD"), cancellable = true)
-	private void dualStance$disableHarmingPartner(Player target, CallbackInfoReturnable<Boolean> cir) {
+	@ModifyReturnValue(method = "canHarmPlayer", at = @At("RETURN"))
+	private boolean dualStance$disableHarmingPartner(boolean original, @Local(argsOnly = true, name = "target") Player target) {
 		for (ItemStack itemStack : this.getInventory()) {
 			if (itemStack.has(DualStanceComponents.LINKED_PLAYER)) {
 				if (itemStack.get(DualStanceComponents.LINKED_PLAYER).id() == target.getUUID()) {
-					cir.setReturnValue(false);
+					return false;
 				} else {
-					cir.cancel();
+					return original;
 				}
 			}
 		}
+		return original;
+	}
+
+	@ModifyVariable(method = "hurtServer", at = @At("HEAD"), name = "damage", argsOnly = true)
+	private float dualStance$disablePacifismDamageTaken(float damage, @Local(argsOnly = true, name = "source") DamageSource source) {
+		if (source.getEntity() != null) {
+			for (ItemStack itemStack : this.getInventory()) {
+				if (itemStack.has(DualStanceComponents.HEART_SEALED_CREST)) {
+					if (itemStack.get(DualStanceComponents.HEART_SEALED_CREST).crest().getItem() == DualStanceItems.PACIFISM_CREST) {
+						return damage * 0;
+					}
+				}
+			}
+		}
+		return damage;
 	}
 }
